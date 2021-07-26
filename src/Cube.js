0484d10e8b4eeb92d992fe6c231974d7d1cead83
Nicolas Castejon <castejon.nicolas@gmail.com>
2021-07-26 20:21:32 +0200

    Small improvements

--- src/Cube.js
+++ src/Cube.js
@@ -1,8 +1,27 @@
 import * as CANNON from "cannon-es";
 import * as THREE from "three";
+import * as Tone from "tone";
 
 export class Cube {
-  constructor(size, color, material = null, position = [0, 0, 0]) {
+  constructor(
+    size,
+    color,
+    material = null,
+    position = [0, 0, 0],
+    musicNote = "G4"
+  ) {
+    this.size = size;
+    this.color = color;
+    this.material = material;
+    this.position = position;
+    this.musicNote = musicNote;
+
+    /**
+     * Sounds
+     */
+    //create a synth and connect it to the main output (your speakers)
+    this.synth = new Tone.Synth().toDestination();
+
     const geometry = new THREE.BoxGeometry();
     const meshMaterial = new THREE.MeshStandardMaterial({
       color
@@ -30,5 +49,17 @@ export class Cube {
 
   updateMeshRotation() {
     this.mesh.quaternion.copy(this.body.quaternion);
+  }
+
+  activate() {
+    this.mesh.material.color.set("yellow");
+    this.synth.triggerAttackRelease(this.musicNote, "8n");
+    this.body.applyImpulse(new CANNON.Vec3(0, 3, 0));
+  }
+
+  deactivate() {
+    console.log(this.color);
+
+    this.mesh.material.color.set(this.color);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import * as Tone from "tone";
 import * as CANNON from "cannon-es";
 import cannonDebugger from "cannon-es-debugger";
 import { Cube } from "./Cube";
@@ -9,12 +8,6 @@ const CUBE_1_COLOR = 0xff3344;
 const CUBE_2_COLOR = 0x2222ff;
 const CUBE_3_COLOR = 0x661166;
 const CUBE_4_COLOR = 0x118811;
-
-/**
- * Sounds
- */
-//create a synth and connect it to the main output (your speakers)
-const synth = new Tone.Synth().toDestination();
 
 const scene = new THREE.Scene();
 
@@ -45,10 +38,10 @@ const controls = new OrbitControls(camera, renderer.domElement);
  * Objects
  */
 const plasticMaterial = new CANNON.Material("plastic");
-const cube1 = new Cube(0.5, CUBE_1_COLOR, plasticMaterial, [-3, 1, 0]);
-const cube2 = new Cube(0.5, CUBE_2_COLOR, plasticMaterial, [-1, 1, 0]);
-const cube3 = new Cube(0.5, CUBE_3_COLOR, plasticMaterial, [1, 1, 0]);
-const cube4 = new Cube(0.5, CUBE_4_COLOR, plasticMaterial, [3, 1, 0]);
+const cube1 = new Cube(0.5, CUBE_1_COLOR, plasticMaterial, [-3, 1, 0], "G4");
+const cube2 = new Cube(0.5, CUBE_2_COLOR, plasticMaterial, [-1, 1, 0], "A4");
+const cube3 = new Cube(0.5, CUBE_3_COLOR, plasticMaterial, [1, 1, 0], "B4");
+const cube4 = new Cube(0.5, CUBE_4_COLOR, plasticMaterial, [3, 1, 0], "C5");
 const cubes = [cube1, cube2, cube3, cube4];
 
 // Add all the meshes to the scene
@@ -95,33 +88,25 @@ cannonDebugger(scene, world.bodies);
 
 document.addEventListener("keydown", (e) => {
   if (e.key === "a") {
-    cube1.mesh.material.color.set("yellow");
-    synth.triggerAttackRelease("G4", "8n");
-    cube1.body.applyImpulse(new CANNON.Vec3(0, 3, 0));
+    cube1.activate();
   } else if (e.key === "z") {
-    cube2.mesh.material.color.set("yellow");
-    synth.triggerAttackRelease("A4", "8n");
-    cube2.body.applyImpulse(new CANNON.Vec3(0, 3, 0));
+    cube2.activate();
   } else if (e.key === "e") {
-    cube3.mesh.material.color.set("yellow");
-    synth.triggerAttackRelease("B4", "8n");
-    cube3.body.applyImpulse(new CANNON.Vec3(0, 3, 0));
+    cube3.activate();
   } else if (e.key === "r") {
-    cube4.mesh.material.color.set("yellow");
-    synth.triggerAttackRelease("C5", "8n");
-    cube4.body.applyImpulse(new CANNON.Vec3(0, 3, 0));
+    cube4.activate();
   }
 });
 
-window.addEventListener("keyup", (e) => {
+document.addEventListener("keyup", (e) => {
   if (e.key === "a") {
-    cube1.mesh.material.color.set(CUBE_1_COLOR);
+    cube1.deactivate();
   } else if (e.key === "z") {
-    cube2.mesh.material.color.set(CUBE_2_COLOR);
+    cube2.deactivate();
   } else if (e.key === "e") {
-    cube3.mesh.material.color.set(CUBE_3_COLOR);
+    cube3.deactivate();
   } else if (e.key === "r") {
-    cube4.mesh.material.color.set(CUBE_4_COLOR);
+    cube4.deactivate();
   }
 });
 
